64525ad87ce3f8dcae398eaa92b83d65808b3b2d
caiyiyunzhan <2216424118@qq.com>
2026-06-22 01:04:10 +0800

    feat: ScheduleCheatsheetCards Cron参考卡片汉化

--- resources/scripts/components/server/schedules/ScheduleCheatsheetCards.tsx
+++ resources/scripts/components/server/schedules/ScheduleCheatsheetCards.tsx
@@ -1,52 +1,55 @@
 import React from 'react';
 import tw from 'twin.macro';
+import { useTranslation } from "react-i18next";
 
 export default () => {
+    const { t } = useTranslation("server");
+
     return (
         <>
             <div css={tw`md:w-1/2 h-full bg-neutral-600`}>
                 <div css={tw`flex flex-col`}>
-                    <h2 css={tw`py-4 px-6 font-bold`}>Examples</h2>
+                    <h2 css={tw`py-4 px-6 font-bold`}>{t("cron_examples")}</h2>
                     <div css={tw`flex py-4 px-6 bg-neutral-500`}>
                         <div css={tw`w-1/2`}>*/5 * * * *</div>
-                        <div css={tw`w-1/2`}>every 5 minutes</div>
+                        <div css={tw`w-1/2`}>{t("cron_every_5min")}</div>
                     </div>
                     <div css={tw`flex py-4 px-6`}>
                         <div css={tw`w-1/2`}>0 */1 * * *</div>
-                        <div css={tw`w-1/2`}>every hour</div>
+                        <div css={tw`w-1/2`}>{t("cron_every_hour")}</div>
                     </div>
                     <div css={tw`flex py-4 px-6 bg-neutral-500`}>
                         <div css={tw`w-1/2`}>0 8-12 * * *</div>
-                        <div css={tw`w-1/2`}>hour range</div>
+                        <div css={tw`w-1/2`}>{t("cron_hour_range")}</div>
                     </div>
                     <div css={tw`flex py-4 px-6`}>
                         <div css={tw`w-1/2`}>0 0 * * *</div>
-                        <div css={tw`w-1/2`}>once a day</div>
+                        <div css={tw`w-1/2`}>{t("cron_once_a_day")}</div>
                     </div>
                     <div css={tw`flex py-4 px-6 bg-neutral-500`}>
                         <div css={tw`w-1/2`}>0 0 * * MON</div>
-                        <div css={tw`w-1/2`}>every Monday</div>
+                        <div css={tw`w-1/2`}>{t("cron_every_monday")}</div>
                     </div>
                 </div>
             </div>
             <div css={tw`md:w-1/2 h-full bg-neutral-600`}>
-                <h2 css={tw`py-4 px-6 font-bold`}>Special Characters</h2>
+                <h2 css={tw`py-4 px-6 font-bold`}>{t("cron_special_chars")}</h2>
                 <div css={tw`flex flex-col`}>
                     <div css={tw`flex py-4 px-6 bg-neutral-500`}>
                         <div css={tw`w-1/2`}>*</div>
-                        <div css={tw`w-1/2`}>any value</div>
+                        <div css={tw`w-1/2`}>{t("cron_any_value")}</div>
                     </div>
                     <div css={tw`flex py-4 px-6`}>
                         <div css={tw`w-1/2`}>,</div>
-                        <div css={tw`w-1/2`}>value list separator</div>
+                        <div css={tw`w-1/2`}>{t("cron_list_separator")}</div>
                     </div>
                     <div css={tw`flex py-4 px-6 bg-neutral-500`}>
                         <div css={tw`w-1/2`}>-</div>
-                        <div css={tw`w-1/2`}>range values</div>
+                        <div css={tw`w-1/2`}>{t("cron_range_values")}</div>
                     </div>
                     <div css={tw`flex py-4 px-6`}>
                         <div css={tw`w-1/2`}>/</div>
-                        <div css={tw`w-1/2`}>step values</div>
+                        <div css={tw`w-1/2`}>{t("cron_step_values")}</div>
                     </div>
                 </div>
             </div>
